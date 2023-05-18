508af4ebaba64f7cad5f82ae698e49cd21d4aa0b
Jocilé Serra <jocile@gmail.com>
2023-05-17 21:08:04 -0300

    Criada a classe Animal.java

---
+++ src/main/java/Animal.java
@@ -0,0 +1,24 @@
+public class Animal {
+    public String Nome;
+    public String Classe;
+    public String Familia;
+    public int Idade;
+    public boolean Estado;
+    public int Caloria;
+    public int Forca;
+
+    public void nacer() {
+    }
+
+    public void morrer() {
+    }
+
+    public void comer() {
+    }
+
+    public void correr() {
+    }
+
+    public void dormir() {
+    }
+}
